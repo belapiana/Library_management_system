3322b68aca467f09febcf46ee8adcfd58dcd0c6a
= <=>
2024-10-17 21:11:59 -0400

    Create Patron class to borrow and return books

--- library_system.js
+++ library_system.js
@@ -13,7 +13,7 @@ this._isAvailable = true
     get isAvailable () {return this._isAvailable}
 
     set changeAvailability (newAvailability) {
-    this._isAvailable = ! this._isAvailable
+    this._isAvailable = newAvailability
 
     }
 }
@@ -27,5 +27,51 @@ class Section {
 
     addBook(newBook) {this._books.push(newBook)}
     getAvailableBooks() {return this._books.filter(book => book.isAvailable).length}
-    listAvailableBooks() {return this._books.map(book => `Title: ${book._title}, Availability: ${book.isAvailable}`)}
+    listBooks() {return this._books.map(book => `Title: ${book._title}, Availability: ${book.isAvailable}`)}
 }
+
+// Task 3:
+
+class Patron {
+    constructor(name) {
+        this._name = name
+        this._borrowedBooks = []
+    }
+    borrowBook(title) {
+        if (title.isAvailable) {
+        this._borrowedBooks.push(title);
+        title.changeAvailability = false }
+        else {return 'This book is currently unavailable'}
+    }
+    returnBook(title) {
+        const index = this._borrowedBooks.indexOf(title)
+        this._borrowedBooks.splice(index, 1)
+        title.changeAvailability = true
+    }
+}
+
+// Task 4: 
+
+// Test data
+
+const drama = new Section ('Drama')
+const romance = new Section ('Romance')
+
+const book1 = new Book ("Cinderella", 'Charles Perrault', 53268329 )
+const book2 = new Book ('Sleeping Beauty', 'Grim Brothers', 632732832)
+const book3 = new Book ('Three Little Pigs', "Joseph Jacobs", 6377382)
+
+drama.addBook(book1)
+drama.addBook(book2)
+romance.addBook(book3)
+
+book1.changeAvailability = true
+
+
+const patron1 = new Patron ('Isabela')
+const patron2 = new Patron ('Manuela')
+
+patron1.borrowBook(book1)
+console.log(patron1._borrowedBooks)
+patron1.returnBook(book1)
+console.log(patron1._borrowedBooks)
